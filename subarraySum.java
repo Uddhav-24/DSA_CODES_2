7a76fba7baf219a44ce5ebc63b09faadf2eef312
Uddhav Davey <108543370+Uddhav-24@users.noreply.github.com>
2023-07-05 22:47:07 +0530

    Add files via upload

---
+++ subarraySum.java
@@ -0,0 +1,27 @@
+// PROBLEM: Subarray Sum Equals K
+
+
+// PLATFORM: LEETCODE
+
+
+// DIFFICULTY:MEDIUM
+
+import java.util.HashMap;
+
+public class subarraySum // Change the CLASS NAME {
+    public int subarraySum(int[] nums, int k) {
+        int ans = 0;
+        int sum = 0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            int diff = sum-k;
+            if(map.containsKey(diff)){
+                ans+=map.get(diff);
+            }
+            map.put(sum,map.getOrDefault(sum,0)+1);
+        }
+        return ans;
+    }
+}

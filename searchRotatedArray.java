96e814cdf7a74ea1435819cad0330c3d1f260ddf
Uddhav Davey <108543370+Uddhav-24@users.noreply.github.com>
2023-07-24 01:02:54 +0530

    Add files via upload

---
+++ searchRotatedArray.java
@@ -0,0 +1,54 @@
+// PROBLEM: Search in Rotated Sorted Array II
+
+
+//PROBLEM LINK: https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+
+// PLATFORM: LEETCODE
+
+
+// DIFFICULTY: MEDIUM
+
+
+
+class Solution {
+
+
+    //Change the CLASS NAME
+
+
+    public boolean search(int[] nums, int target) {
+        int low=0;
+        int high=nums.length-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+
+            if(nums[mid]==target) return true;
+
+            if(nums[low]==nums[mid] && nums[mid]==nums[high]){
+                low+=1;
+                high-=1;
+                continue;
+            }
+
+            if(nums[low]<=nums[mid]){
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
+                }
+                else{
+                    low=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}

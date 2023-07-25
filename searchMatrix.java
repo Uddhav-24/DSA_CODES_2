f7b08746675e486774f5136b77b5c08750c22040
Uddhav Davey <108543370+Uddhav-24@users.noreply.github.com>
2023-07-26 00:14:49 +0530

    Add files via upload

---
+++ searchMatrix.java
@@ -0,0 +1,41 @@
+// PROBLEM: Search a 2D Matrix
+
+
+//PROBLEM LINK: https://leetcode.com/problems/search-a-2d-matrix/
+
+
+// PLATFORM: LEETCODE
+
+
+// DIFFICULTY: MEDIUM
+
+
+class Solution {
+
+
+    //Change the CLASS NAME
+
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int low = 0;
+        if(matrix.length == 0)
+            return false;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int high = (n*m) - 1;
+
+        while(low <= high){
+            int mid = (low + (high - low)/2);
+            if(matrix[mid/m][mid % m] == target) {
+                return true;
+            }
+            if(matrix[mid/m][mid % m] < target) {
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
+}

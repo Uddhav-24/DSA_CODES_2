1d44f5d909caaf9802a83602e7b401fb202cdeb9
Uddhav Davey <108543370+Uddhav-24@users.noreply.github.com>
2023-07-30 17:33:16 +0530

    Add files via upload

---
+++ rotateString.java
@@ -0,0 +1,31 @@
+// PROBLEM: Rotate String
+
+
+//PROBLEM LINK: https://leetcode.com/problems/rotate-string/
+
+
+// PLATFORM: LEETCODE
+
+
+// DIFFICULTY: EASY
+
+
+class Solution {
+    
+
+    //Change the CLASS NAME
+
+
+    public boolean rotateString(String s, String goal) {
+        if (s.length() != goal.length()) return false;
+        if (s.equals(goal)) return true;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < s.length(); i++) {
+            sb.append(s.substring(1)).append(s.charAt(0));
+            s = sb.toString();
+            if (s.equals(goal)) return true;
+            sb.setLength(0);
+        }
+        return false;
+    }
+}

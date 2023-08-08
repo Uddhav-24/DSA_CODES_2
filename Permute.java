09c6160955fc179eb4e412fce73a99e9e14a2d7a
Uddhav Davey <108543370+Uddhav-24@users.noreply.github.com>
2023-08-08 16:59:51 +0530

    Add files via upload

---
+++ Permute.java
@@ -0,0 +1,40 @@
+// PROBLEM: Permutations
+
+
+//PROBLEM LINK: https://leetcode.com/problems/permutations/
+
+
+// PLATFORM: LEETCODE
+
+
+// DIFFICULTY: MEDIUM
+
+
+class Solution {
+
+
+    //Change the CLASS NAME
+
+
+    List<List<Integer>> result;
+    public List<List<Integer>> permute(int[] nums) {
+        result = new ArrayList<>();
+        helper(new ArrayList<>(), nums);
+        return result;
+    }
+
+    public void helper(List<Integer> curr, int[] nums) {
+        if (curr.size() == nums.length) {
+            result.add(new ArrayList<>(curr));
+            return;
+        }
+
+        for (int num: nums) {
+            if (!curr.contains(num)) {
+                curr.add(num);
+                helper(curr, nums);
+                curr.remove(curr.size() - 1);
+            }
+        }
+    }
+}

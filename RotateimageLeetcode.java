6c22bf7e0a5ce29de196ffb5e60781bf0b62ce0a
Uddhav Davey <108543370+Uddhav-24@users.noreply.github.com>
2023-09-16 18:14:24 +0530

    RotateimageLeetcode.java

--- RotateimageLeetcode.java
+++ RotateimageLeetcode.java
@@ -11,6 +11,11 @@
 
 
 class Scratch{
+
+
+    //Change the CLASS NAME
+
+    
     public static void main(String[] args) {
         int[][] matrix =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
         rotate(matrix);
